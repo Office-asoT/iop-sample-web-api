1ba548b2cf0f52afa346aa00959bc6b634295f96
Takaya Matsumoto <mini.milktea60@gmail.com>
2025-02-07 18:21:05 +0900

    path修正

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from api import views
 urlpatterns = [
     path('municipalities', views.MunicipalityList.as_view()),
     path('weather_forecast_settings/<str:user_id>', views.WeatherForecastSettingDetail.as_view()),
-    path('farm-field/<str:user_id>', views.FarmFieldList.as_view()),
+    path('farm_fields/<str:user_id>', views.FarmFieldList.as_view()),
 ]
